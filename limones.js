235b5b409d2dddd4a413c1cf82885d5dd143f455
RyanReynaS <ryanedurs17@gmail.com>
2026-09-08 14:00:21 -0500

    agregar dibujarSuelo y dibujarPersonaje

--- limones.js
+++ limones.js
@@ -1,2 +1,18 @@
 let canvas = document.getElementById("areaJuego");
 let ctx = canvas.getContext("2d");
+const ALTURA_SUELO = 20;
+const ALTURA_PERSONAJE = 60;
+
+function iniciar() {
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+
+function dibujarSuelo() {
+    ctx.fillStyle = "#a2e6f2";
+    ctx.fillRect(0, canvas.height - ALTURA_SUELO, canvas.width, ALTURA_SUELO);
+}
+function dibujarPersonaje(){
+    ctx.fillStyle = "#528f18";
+    ctx.fillRect(canvas.width / 2 - 25, canvas.height - (ALTURA_SUELO + ALTURA_PERSONAJE), 40, ALTURA_PERSONAJE);
+}
