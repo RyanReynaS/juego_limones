075f2452525490561d7b9e229f1eb720101425e0
RyanReynaS <ryanedurs17@gmail.com>
2026-09-09 09:01:37 -0500

    agregarr bajar limon

--- limones.js
+++ limones.js
@@ -1,13 +1,19 @@
 let canvas = document.getElementById("areaJuego");
 let ctx = canvas.getContext("2d");
+
 const ALTURA_SUELO = 20;
 const ALTURA_PERSONAJE = 60;
-let personajeX = canvas.width / 2;
+const ANCHO_LIMON=20;
+const ALTO_LIMON=20;
 
+let personajeX = canvas.width / 2;
+let limonX=canvas.width/2;
+let limonY=0;
 
 function iniciar() {
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimon();
 }
 
 function dibujarSuelo() {
@@ -15,7 +21,7 @@ function dibujarSuelo() {
     ctx.fillRect(0, canvas.height - ALTURA_SUELO, canvas.width, ALTURA_SUELO);
 }
 function dibujarPersonaje(){
-    ctx.fillStyle = "#528f18";
+    ctx.fillStyle = "#18568f";
     ctx.fillRect(personajeX, canvas.height - (ALTURA_SUELO + ALTURA_PERSONAJE), 40, ALTURA_PERSONAJE);
 }
 
@@ -35,9 +41,20 @@ function actualizarPantalla(){
     limpiarCanva();
     dibujarPersonaje();
     dibujarSuelo();
+    dibujarLimon(); // CORREGIDO: Agregados paréntesis para llamar la función
 }
 
 
 function limpiarCanva(){
     ctx.clearRect(0,0,canvas.width, canvas.height);
 }
+
+function dibujarLimon(){
+     ctx.fillStyle = "#4e8f18";
+     ctx.fillRect(limonX, limonY, ANCHO_LIMON, ALTO_LIMON); // CORREGIDO: Cambié punto por coma entre limonY y ANCHO_LIMON, agregué coma antes de ALTO_LIMON
+}
+function bajarLimon(){
+    limonY = limonY + 10;
+    actualizarPantalla(); // CORREGIDO: Agregados paréntesis para llamar la función
+}
+   
